7e75f6def141467af8ccb541ae0f4a69fb220b9d
sownt <thonsai@outlook.com>
2023-05-08 11:19:52 +0700

    update sizes

---
+++ site/lib/util/font_size.dart
@@ -0,0 +1,10 @@
+class FontSize {
+  FontSize._();
+
+  static const h1 = 96;
+  static const h2 = 60;
+  static const h3 = 48;
+  static const h4 = 34;
+  static const h5 = 24;
+  static const h6 = 20;
+}
